fdba06f4ba1e885a381831a261c558c22c8751f7
joaocairiton <joaocairiton@gmail.com>
2023-05-19 20:54:37 -0300

    testando circuit breaker

--- book-service/src/main/java/com/cairiton/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/cairiton/bookservice/controller/FooBarController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("book-service")
@@ -17,12 +17,17 @@ public class FooBarController {
 	private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 	
 	@GetMapping("/foo-bar")
-	@Retry(name="foo-bar")
+	//@Retry(name="foo-bar", fallbackMethod = "fallbackMethod")
+	@CircuitBreaker(name="default", fallbackMethod = "fallbackMethod")
 	public String foobar() {
 		logger.info("Request to foo bar is received");
 		var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
 		//return "foo-bar";
 		return response.getBody();
 	} 
+	
+	public String fallbackMethod (Exception ex) {
+		return "fallbackMethod foo-bar!!!";
+	}
 
 }
